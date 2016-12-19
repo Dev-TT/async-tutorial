436fab6b8f4a126abe02ff3efdfe846a27c360c7
Dev-TT <juangh2@gmail.com>
2016-12-19 07:45:44 -0400

    Initial commit

---
+++ async-tutorial.js
@@ -0,0 +1,68 @@
+var async = require ("async");
+
+///////////// GIVE FUNCTIONS AS ARRAY
+///////////// RETURNS: ARRAY
+
+var stack=[];
+f=1;
+
+var function1 = function (callback){	// The callback here is compulsory, even if no callbck is defined below
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F1");				// No Error. Returns Value to the parallel: "F1"
+}
+
+var function2 = function (callback){
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F2");
+	//callback ("ERROR", null);			// returning an error at this moment, the final result will be: [ 'F1', null ], because it stops executing the additional functions
+}
+
+var function3 = function (callback){
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F3");
+}
+
+stack.push(function1);
+stack.push(function2);
+stack.push(function3);
+
+async.series(stack, function(err, result){		// the second parameter (the callback) is OPTIONAL, BUT I have found that without the callback, then the script does not stop on error (in parallel). in series it stops
+	console.log("This is the callback: ");						// returns array: [ 'F1', 'F2', 'F3' ]
+	console.log(result);
+});
+
+
+///////////// GIVE FUNCTIONS AS OBJECT
+///////////// RETURNS: OBJECT
+
+var stack={};
+f=1;
+
+stack.function1 = function (callback){
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F1");
+
+	//callback ("ERROR", null);	
+}
+
+stack.function2 = function (callback){
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F2");
+	//callback ("ERROR", null);			// error at this moment makes the following final result: { function1: 'F1', function2: null }
+}
+
+stack.function3 = function (callback){
+	console.log("f value: "+f);
+	f=f+1;
+	callback (null, "F3");
+}
+
+async.parallel(stack, function(err, result){
+	console.log("This is the callback: ");		// returns object: { function1: 'F1', function2: 'F2', function3: 'F3' }
+	console.log(result);
+});
